d0b85147a82e25c77d5af53d6e631d82013101a4
mahmutunder <mahmutunder@gmail.com>
2023-01-10 23:19:23 -0700

    created menuBar frame

---
+++ src/MenuBar.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuBar extends JFrame implements ActionListener {
+
+
+    public MenuBar(){
+
+        setTitle("Menu Bar");
+        setDefaultCloseOperation(3);
+        setSize(600,600);
+        setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
